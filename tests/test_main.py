dd0939bf5a7381d696c9b24ca1843d99fd533d11
Almaz Muzafarov <muzafrov.almaz2012@yandex.ru>
2024-03-14 17:03:44 +0400

    не совсем понимаю, какими тестами можно покрыть данные функции

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,20 +6,11 @@ import json
 with open('operations.json', 'rt', encoding='utf-8') as file:
     list_transactions = json.load(file)
 
-executed_list = returns_executed_transactions(list_transactions)
-finaly_list=returns_list_last_transactions(executed_list)
 
-
-def test_returns_executed_transactions(list_transactions):
-    assert len(returns_executed_transactions(list_transactions)) !=0
+def test_returns_executed_transactions(executed_list):
+    data = list_transactions
+    assert len(list(filter(returns_executed_transactions, data))) == 10
 
 
 def test_returns_list_last_transactions(executed_list):
-    assert len (returns_executed_transactions(executed_list)) == 5
-
-
-
-
-
-
-
+    assert len(returns_executed_transactions(executed_list)) == 5
